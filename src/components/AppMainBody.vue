faa2db16fa517b321fcd688b278778fd1afd467f
longo2002 <lombardo.fra2002@gmail.com>
2023-03-06 16:56:14 +0100

    Attempt 2

--- src/components/AppMainBody.vue
+++ src/components/AppMainBody.vue
@@ -4,25 +4,48 @@
     </div>
     <div class="container-cards">
         <div class="card">
-            <div class="card-img">
-
+            <div class=" card-img">
+                <img :src="cardsImg" alt="Card_Img">
             </div>
             <div class="card-description">
-
+                <ul class="characteristics">
+                    <li>{{ cardsName }}</li>
+                    <li>{{ cardsType }}</li>
+                </ul>
             </div>
         </div>
 
     </div>
 </template>
 <script>
+import axios from 'axios'
 
 export default {
     data() {
         return {
-
+            cardsImg: "",
+            cardsName: "",
+            cardsType: "",
+            cardNum: 0,
+            cardImgNum: 0,
         }
+    },
+    methods: {
+        cardsNameRequest() {
+            axios
+                .get("https://db.ygoprodeck.com/api/v7/cardinfo.php")
+                .then((res) => {
+                    console.log(res.data.data[this.cardNum].card_images)
+                    this.cardsName = res.data.data[this.cardNum].name
+                    this.cardsType = res.data.data[this.cardNum].type
+                    this.cardsImg = res.data.data[this.cardNum].card_images[this.cardImgNum].image_url_cropped
+                })
+            this.cardNum++
+        }
+    },
+    created: function () {
+        this.cardsNameRequest()
     }
-
 }
 
 </script>
@@ -57,5 +80,25 @@ export default {
 
 .card-description {
     height: calc(100% - 250px);
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.characteristics {
+    color: black;
+    padding: 0;
+    margin: 0;
+}
+
+.characteristics li:first-child {
+    font-weight: 800;
+    text-transform: uppercase;
+    font-size: 12px;
+}
+
+.characteristics li:last-child {
+    text-align: center;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
-import axios from 'axios'
 import App from './App.vue'
 
 createApp(App).mount('#app')
-
-const url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Tornado%20Dragon"
-
-
